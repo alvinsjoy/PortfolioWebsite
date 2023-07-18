8528d30073af959cc2f25bd1ac61396ec1c829e6
Alvin Joy <89687635+AlvinJoyDev@users.noreply.github.com>
2023-07-18 11:41:16 +0530

    Portfolio v2 using React

---
+++ src/components/Home/Type.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Human",
+          "Software Developer",
+          "Freelancer",
+          "JavaScript Developer",
+          "CSE Undergrad",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
