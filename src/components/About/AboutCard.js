26f1af36c1482e7c9f2cb37bbf09898a7da2be7b
Alvin Joy <89687635+AlvinJoyDev@users.noreply.github.com>
2023-07-18 18:42:46 +0530

    Update color

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,8 +8,8 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Alvin Joy </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="blue">Alvin Joy </span>
+            from <span className="blue"> Bhubaneswar, India.</span>
             <br /> I am a final year student pursuing an Integrated MSc (IMSc)
             in Maths and Computing at BIT Mesra.
             <br />
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -16,7 +16,7 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            <strong className="blue">Introduction</strong>
             </h1>
             <p className="home-about-body">
               I fell in love with programming and I have at least learnt
@@ -24,31 +24,31 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="blue"> C++, Javascript and Go. </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="blue">Web Technologies and Products </b> and
                 also in areas related to{" "}
-                <b className="purple">
+                <b className="blue">
                   Blockchain.
                 </b>
               </i>
               <br />
               <br />
               Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              with <b className="blue">Node.js</b> and
               <i>
-                <b className="purple">
+                <b className="blue">
                   {" "}
                   Modern Javascript Library and Frameworks
                 </b>
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="blue"> React.js and Next.js</b>
               </i>
             </p>
           </Col>
@@ -60,9 +60,11 @@ function Home2() {
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
-            <h1>SOCIALS</h1>
+            <h1>
+            <strong className="blue">Socials</strong>
+            </h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              Feel free to <span className="blue">connect </span>with me
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
