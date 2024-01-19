74ba77f7ed672b6af70392fad61279d9c88df4f5
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-01-19 11:36:00 +0530

    Testing custom cursor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -23,6 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
       <Component {...pageProps} />
+      <SpeedInsights />
     </>
   );
 }
