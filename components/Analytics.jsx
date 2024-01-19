d5c8e8fe278f60246196a83258164f8211c10d3d
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-01-19 16:43:55 +0530

    Refactor Analytics component to use next/script for script loading

--- components/Analytics.jsx
+++ components/Analytics.jsx
@@ -1,21 +1,17 @@
-import Head from 'next/head';
+import Script from 'next/script';
 
 const Analytics = () => (
-  <Head>
-    <>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-VRB81RLGRT"></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-VRB81RLGRT');
-          `,
-        }}
-      />
-    </>
-  </Head>
+  <>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VRB81RLGRT" />
+    <Script id="ga-script" strategy="lazyOnload">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-VRB81RLGRT');
+      `}
+    </Script>
+  </>
 );
 
 export default Analytics;
