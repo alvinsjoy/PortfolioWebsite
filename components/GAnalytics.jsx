22d33287dacfd9286a820e0084b1027a4756f946
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-01-26 14:18:40 +0530

    Refactor components and update dependencies

--- components/GAnalytics.jsx
+++ components/GAnalytics.jsx
@@ -1,17 +1,17 @@
 import Script from 'next/script';
 
-const GAnalytics = () => (
-  <>
-    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VRB81RLGRT" />
-    <Script id="ga-script" strategy="lazyOnload">
-      {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-VRB81RLGRT');
-      `}
-    </Script>
-  </>
-);
-
-export default GAnalytics;
+export default function GAnalytics() {
+  return (
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VRB81RLGRT" />
+      <Script id="ga-script" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-VRB81RLGRT');
+        `}
+      </Script>
+    </>
+  );
+}
