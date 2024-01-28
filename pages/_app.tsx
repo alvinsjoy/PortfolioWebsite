3907d33ff7d75ad78e794f1379702dd02d1484d4
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-01-28 17:06:50 +0530

    Delete unused components and update dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,37 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import GAnalytics from "../components/GAnalytics";
+import GAnalytics from "@/components/GAnalytics";
 import { Analytics } from "@vercel/analytics/react";
+import gsap from "gsap";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const cursorRef = React.useRef(null);
+
+  React.useEffect(() => {
+    document.addEventListener("mousemove", (e) => {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+      gsap.to(cursorRef.current, {
+        x: mouseX,
+        y: mouseY,
+        opacity: 1,
+        delay: 0,
+      });
+    });
+
+    const hideCursor = () => {
+      gsap.to(cursorRef.current, { opacity: 0 });
+    };
+
+    const showCursor = () => {
+      gsap.to(cursorRef.current, { opacity: 1 });
+    };
+
+    document.addEventListener("mouseleave", hideCursor);
+    document.addEventListener("mousedown", hideCursor);
+    document.addEventListener("mouseup", showCursor);
+  }, []);
   return (
     <>
       <Head>
@@ -23,6 +50,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/icon.ico" type="image/x-icon" />
         <link rel="icon" href="/assets/icon.ico" type="image/x-icon" />
       </Head>
+      <div
+        ref={cursorRef}
+        className="hidden lg:block w-12 h-12 opacity-0 pointer-events-none rounded-full border-2 border-marrsgreen dark:border-carrigreen z-[9999] fixed -translate-x-1/2 -translate-y-1/2"
+      />
       <GAnalytics />
       <Component {...pageProps} />
       <Analytics />
