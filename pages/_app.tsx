ac57df9a0672e8688c920cfbc15f2b9351423ee9
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-02-05 19:30:40 +0530

    Update social media image links and images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -53,7 +53,7 @@ export default function App({ Component, pageProps }: AppProps) {
         {/* Twitter */ }
         <meta name="twitter:title" content="Profile | Alvin Joy" />
         <meta name="twitter:description" content="JavaScript developer creating interactive apps, skilled in discord.js and exploring Flutter and web development. Eager learner, excited about tech's future." />
-        <meta name="twitter:image:src" content="/assets/Screenshot.jpg" />
+        <meta name="twitter:image:src" content="/assets/Mac.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@_alvinjoy_" />
         {/* Open Graph (Facebook) */}
@@ -61,7 +61,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Profile | Alvin Joy" />
         <meta property="og:description" content="JavaScript developer creating interactive apps, skilled in discord.js and exploring Flutter and web development. Eager learner, excited about tech's future." />
-        <meta property="og:image" content="/assets/Screenshot.jpg" />
+        <meta property="og:image" content="/assets/Mac.jpg" />
         {/* Add more meta tags as needed */}
 
         {/* Favicon (example assuming you have a favicon.ico file in public directory) */}
