df8b51e09ce3fd47e4bf51bf72de94762e9bce27
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-05-11 21:33:48 +0530

    chore: Update Google Analytics tracking ID

--- components/GAnalytics.tsx
+++ components/GAnalytics.tsx
@@ -5,14 +5,14 @@ export default function GAnalytics() {
     <>
       <Script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-VRB81RLGRT"
+        src="https://www.googletagmanager.com/gtag/js?id=G-0VETRJFPDN"
       />
       <Script id="ga-script" strategy="lazyOnload">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-VRB81RLGRT');
+          gtag('config', 'G-0VETRJFPDN');
         `}
       </Script>
     </>
