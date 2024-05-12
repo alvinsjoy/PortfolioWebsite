c53bf6011e416bafff277c973dddfbb9ee9e6ec9
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-05-12 14:37:46 +0530

    chore: Fix indentation and use default export syntax in GAnalytics and ThemeButton components

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -3,22 +3,21 @@ import React from 'react';
 import { useTheme } from 'next-themes';
 import { DarkMode, LightMode } from '@mui/icons-material';
 import { motion } from 'framer-motion';
-
-const ThemeButton = ({ index }: any | null) => {
+export default function ThemeButton(){
+  const ThemeButton = ({ index }: any | null) => {
     const { systemTheme, theme, setTheme } = useTheme();
     const currentTheme = theme === 'system' ? systemTheme : theme;
 
     return (
-        <motion.button
-            initial={{ y: -50, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={index && { duration: 0.3 * index }}
-            onClick={() => (theme == 'dark' ? setTheme('light') : setTheme('dark'))}
-            className="hover:bg-gray-300 dark:hover:bg-gray-700 transition-all rounded flex items-center justify-center h-7 w-7"
-        >
-            {currentTheme === 'dark' ? <DarkMode /> : <LightMode />}
-        </motion.button>
+      <motion.button
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={index && { duration: 0.3 * index }}
+        onClick={() => (theme == 'dark' ? setTheme('light') : setTheme('dark'))}
+        className="hover:bg-gray-300 dark:hover:bg-gray-700 transition-all rounded flex items-center justify-center h-7 w-7"
+      >
+        {currentTheme === 'dark' ? <DarkMode /> : <LightMode />}
+       </motion.button>
     );
+  };
 };
-
-export default ThemeButton;
