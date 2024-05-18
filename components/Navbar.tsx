bb26e7e57fd05ec785577f79b51af37f02c4d1aa
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-05-18 21:33:42 +0530

    refactor: Update Navbar and index styles for light and dark mode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const {
 } = jsonData;
 export default function Navbar() {
   return (
-    <header className="fixed top-4 left-1/2 z-50 w-[90%] max-w-[1200px] -translate-x-1/2 rounded-3xl backdrop-blur-md bg-gray-950/60 shadow-lg">
+    <header className="fixed top-4 left-1/2 z-50 w-[90%] max-w-[1200px] -translate-x-1/2 rounded-3xl backdrop-blur-md bg-gray-200/60 dark:bg-gray-950/60 shadow-lg">
       <div className="container flex h-16 items-center justify-between px-4 md:px-6">
         <Link href="/">
           <Avatar className="h-9 w-9 rounded-full">
@@ -21,19 +21,19 @@ export default function Navbar() {
         </Link>
         <nav className="flex items-center space-x-4">
           <Link
-            className="text-sm font-medium text-gray-50 hover:text-gray-200 hover:underline"
+            className="text-sm font-medium text-gray-900 dark:text-gray-50 hover:text-gray-800 dark:hover:text-gray-200 hover:underline"
             href="/"
           >
             Home
           </Link>
           <Link
-            className="text-sm font-medium text-gray-50 hover:text-gray-200 hover:underline"
+            className="text-sm font-medium text-gray-900 dark:text-gray-50 hover:text-gray-800 dark:hover:text-gray-200 hover:underline"
             href="#"
           >
             About
           </Link>
           <Link
-            className="text-sm font-medium text-gray-50 hover:text-gray-200 hover:underline"
+            className="text-sm font-medium text-gray-900 dark:text-gray-50 hover:text-gray-800 dark:hover:text-gray-200 hover:underline"
             href={github.link}
             target="_blank"
             rel="noopener noreferrer"
