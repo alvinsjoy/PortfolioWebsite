16ee491d390fa3c04cb4dd35171e02a3fa6c9b21
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-05-19 15:02:35 +0530

    refactor: Update background gradient colors in index page for light mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ export default function MyComponent() {
   return (
     <div
       key="1"
-      className={`bg-gradient-to-r from-[#d066c9] via-gray-200 to-[#bf8343] text-black dark:from-[#3f203d] dark:via-black dark:to-[#2f2010] dark:text-white min-h-screen flex flex-col justify-center items-center ${
+      className={`bg-gradient-to-r from-[#c761c0] via-gray-200 to-[#b77d40] text-black dark:from-[#3f203d] dark:via-black dark:to-[#2f2010] dark:text-white min-h-screen flex flex-col justify-center items-center ${
         isVisible || !animationFinished ? 'visible' : 'hidden'
       }`}
     >
@@ -47,14 +47,14 @@ export default function MyComponent() {
       <h1 className="gradient-text text-transparent bg-clip-text text-center">
         {name}
       </h1>
-      <h2 className="text-4xl font-light mb-4 bg-gradient-to-r from-gray-900 via-gray-500 to-gray-300 dark:from-gray-200 dark:via-gray-500 dark:to-gray-900 text-transparent bg-clip-text">
+      <h2 className="text-4xl font-bold mb-4 bg-gradient-to-r from-gray-900 via-gray-600 to-gray-300 dark:from-gray-100 dark:via-gray-400 dark:to-gray-700 text-transparent bg-clip-text">
         <Type />
       </h2>
-      <hr className="border-gray-900 dark:border-gray-700 w-full max-w-lg mb-8" />
+      <hr className="border-gray-900 dark:border-gray-600 w-full max-w-lg mb-8" />
       <p className="mb-8 text-center max-w-lg text-[#111111] dark:text-[#AEB2B6]">
         {bio}
       </p>
-      <hr className="border-gray-900 dark:border-gray-700 w-full max-w-lg mb-8" />
+      <hr className="border-gray-900 dark:border-gray-600 w-full max-w-lg mb-8" />
       {/*<h4 className="text-4xl font-light mb-4 bg-gradient-to-r from-white via-gray-500 to-gray-900 text-transparent bg-clip-text">
         Skills
       </h4>
@@ -121,7 +121,7 @@ export default function MyComponent() {
 
       <footer className="w-full text-center p-4 fixed bottom-0">
         <p>
-          <span className="font-aquire bg-gradient-to-r from-gray-600 via-gray-800 to-black dark:from-gray-500 dark:via-gray-600 dark:to-white text-transparent bg-clip-text">
+          <span className="font-aquire bg-gradient-to-r from-gray-500 via-gray-400 to-black dark:from-gray-500 dark:via-gray-600 dark:to-white text-transparent bg-clip-text">
             Copyright ©️ {new Date().getFullYear()} Alvin Joy
           </span>
         </p>
