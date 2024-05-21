69dc1da262ce181926389cf8edd2053aab2847f5
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-05-21 23:11:55 +0530

    refactor: Update ThemeSwitcher icons with hover colors

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -21,8 +21,8 @@ export default function Theme() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 hover:text-[#EE4B2B]" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:hover:text-[#FFEA00]" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
