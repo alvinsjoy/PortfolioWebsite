ffcbb21d8f0f3ae435443695054de412e069cb80
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-06-09 23:31:30 +0530

    refactor: update Navbar styles to use global styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const {
 } = jsonData;
 export default function Navbar() {
   return (
-    <header className="fixed top-4 left-1/2 z-50 w-[90%] max-w-[1200px] -translate-x-1/2 rounded-3xl backdrop-blur-md bg-gray-200/60 dark:bg-gray-950/60 shadow-lg">
+    <header className="fixed top-4 left-1/2 z-50 w-[90%] max-w-[1200px] -translate-x-1/2 rounded-3xl backdrop-blur-sm bg-gray-200/60 dark:bg-gray-950/60 shadow-lg">
       <div className="container flex h-16 items-center justify-between px-4 md:px-6">
         <Link href="/">
           <Avatar className="h-9 w-9 rounded-full">
@@ -22,26 +22,26 @@ export default function Navbar() {
         </Link>
         <nav className="flex items-center gap-4">
           <Link
-            className="text-sm font-medium text-gray-600 dark:text-gray-400 hover:text-[#EE4B2B] dark:hover:text-[#FFEA00]"
+            className="text-sm font-medium text-muted-foreground hover:text-[#EE4B2B] dark:hover:text-[#FFEA00]"
             href="/"
           >
             Home
           </Link>
           <Link
-            className="text-sm font-medium text-gray-600 dark:text-gray-400 hover:text-[#EE4B2B] dark:hover:text-[#FFEA00]"
+            className="text-sm font-medium text-muted-foreground hover:text-[#EE4B2B] dark:hover:text-[#FFEA00]"
             href="#"
           >
             About
           </Link>
           <Link
-            className="text-sm font-medium text-gray-600 dark:text-gray-400 hover:text-[#EE4B2B] dark:hover:text-[#FFEA00]"
+            className="text-sm font-medium text-muted-foreground hover:text-[#EE4B2B] dark:hover:text-[#FFEA00]"
             href={github.link}
             target="_blank"
             rel="noopener noreferrer"
           >
             Contact
           </Link>
-          <Separator className="h-5 w-px bg-gray-500 dark:bg-gray-500" />
+          <Separator className="h-5 w-px bg-muted-foreground" />
           <Theme />
         </nav>
       </div>
