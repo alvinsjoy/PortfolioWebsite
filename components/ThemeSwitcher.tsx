0e2d52214e216cf7e8c7e7b13467cfb50a3e0e60
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-06-24 11:30:20 +0530

    chore: optimize ThemeSwitcher component and add animations

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import * as React from 'react';
 import { MoonIcon, SunIcon, LaptopIcon } from '@radix-ui/react-icons';
 import { useTheme } from 'next-themes';
 
@@ -21,21 +20,30 @@ export default function Theme() {
         <Button
           variant="ghost"
           size="icon"
-          className=" text-muted-foreground hover:text-foreground dark:hover:text-foreground"
+          className="text-muted-foreground hover:text-foreground dark:hover:text-foreground"
         >
-          <SunIcon className="h-[1.2rem] w-[1.2rem] scale-100 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] scale-0 dark:scale-100" />
+          <SunIcon className="absolute w-[1.2rem] h-[1.2rem] rotate-0 scale-1000 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="w-[1.2rem] h-[1.2rem] rotate-90 scale-0 transition-transform ease-in-out duration-500 dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
+        <DropdownMenuItem
+          onClick={() => setTheme('light')}
+          className="text-muted-foreground"
+        >
           <SunIcon className="mr-2 h-4 w-4" /> Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
+        <DropdownMenuItem
+          onClick={() => setTheme('dark')}
+          className="text-muted-foreground"
+        >
           <MoonIcon className="mr-2 h-4 w-4" /> Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
+        <DropdownMenuItem
+          onClick={() => setTheme('system')}
+          className="text-muted-foreground"
+        >
           <LaptopIcon className="mr-2 h-4 w-4" /> System
         </DropdownMenuItem>
       </DropdownMenuContent>
