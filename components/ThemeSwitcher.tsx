d6eef2e1efbd362eb3a14fcb752a19c08d06f774
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-07-02 20:09:51 +0530

    refractor:  set scale to a reasonable value

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -22,7 +22,7 @@ export default function Theme() {
           size="icon"
           className="text-muted-foreground hover:text-foreground dark:hover:text-foreground"
         >
-          <SunIcon className="absolute w-[1.2rem] h-[1.2rem] rotate-0 scale-1000 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0" />
+          <SunIcon className="absolute w-[1.2rem] h-[1.2rem] rotate-0 scale-100 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="w-[1.2rem] h-[1.2rem] rotate-90 scale-0 transition-transform ease-in-out duration-500 dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
