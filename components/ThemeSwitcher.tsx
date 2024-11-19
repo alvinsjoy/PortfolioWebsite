1cc354be74fb1983ac87e2aaf0920854232b1f76
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2024-11-19 18:38:40 +0530

    refactor: replace @radix-ui/react-icons with react-icons for theme switcher

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { RxMoon, RxSun } from 'react-icons/rx';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
@@ -15,8 +15,8 @@ export default function Theme() {
       className="text-muted-foreground hover:text-foreground dark:hover:text-foreground"
       onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
     >
-      <SunIcon className="absolute w-[1.2rem] h-[1.2rem] rotate-0 scale-100 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0" />
-      <MoonIcon className="w-[1.2rem] h-[1.2rem] rotate-90 scale-0 transition-transform ease-in-out duration-500 dark:rotate-0 dark:scale-100" />
+      <RxSun className="absolute w-[1.2rem] h-[1.2rem] rotate-0 scale-100 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0" />
+      <RxMoon className="w-[1.2rem] h-[1.2rem] rotate-90 scale-0 transition-transform ease-in-out duration-500 dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
